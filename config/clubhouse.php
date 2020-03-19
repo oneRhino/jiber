ee5971104a3cfd70c5e7e73793d5dbbfe8395a09
Cortela <cortela@gmail.com>
2020-03-19 11:37:00 -0300

    #18597 Added PM and DEV users to Clubhouse/Redmine user.

--- config/clubhouse.php
+++ config/clubhouse.php
@@ -2,5 +2,6 @@
 
 return [
     'api_key' => '5e32e7c2-797e-4715-b069-d4d5c84bee17',
-    'clubhouse_user' => 'test_user',
+    'clubhouse_dev_user' => 'dev_user',
+    'clubhouse_pm_user' => 'pm_user',
 ];
