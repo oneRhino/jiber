cee6b28ba74e61a7e754024c25bd6fad11d728e0
Thaissa Mendes <thaissa.mendes@gmail.com>
2020-04-09 10:35:43 -0300

    Fix bug when creating redmine user without jira, and use admin when onerhinodev clubhouse user comes from clubhouse

--- app/Http/Controllers/ClubhouseController.php
+++ app/Http/Controllers/ClubhouseController.php
@@ -281,6 +281,10 @@ class ClubhouseController extends Controller {
             if ('geisermenoia' === $redmineClubhouseUserObj->clubhouse_name) {
                 $redmine_user = 'geiser';
             }
+            // If onerhinodev, get billy's user
+            elseif ('onerhinodev' === $redmineClubhouseUserObj->clubhouse_name) {
+                $redmine_user = 'admin';
+            }
         }
 
         // Get user settings
@@ -294,11 +298,11 @@ class ClubhouseController extends Controller {
 
         return $user;
     }
-    
+
     private function getRedmineAssignToUser ($clubhouseUserId) {
-        
+
         $redmineClubhouseUserObj = RedmineClubhouseUser::where('clubhouse_user_permissions_id', $clubhouseUserId)->first();
-        
+
         if (!$redmineClubhouseUserObj->redmine_names) {
             throw new Exception("User {$redmineClubhouseUserObj->clubhouse_name} does not have a redmine user associated.");
         }
@@ -306,10 +310,10 @@ class ClubhouseController extends Controller {
         $redmine_names = json_decode($redmineClubhouseUserObj->redmine_names);
         $redmine_user  = reset($redmine_names); // First user by default
 
-        // If assigned to 'onerhinodev' assigns ticket to Alejandro on Redmine. 
+        // If assigned to 'onerhinodev' assigns ticket to Alejandro on Redmine.
         $onerhinodevUserId = RedmineClubhouseUser::where('clubhouse_name', 'onerhinodev')->first();
         if ($clubhouseUserId == $onerhinodevUserId->clubhouse_user_permissions_id) {
-            $redmine_user = 'alejandro'; 
+            $redmine_user = 'alejandro';
         }
 
         // Get user settings
@@ -468,7 +472,7 @@ class ClubhouseController extends Controller {
             if ($redmineProjectObj->content) {
                 $redmineCreateIssueObj['description'] .= "\n\n" . $redmineProjectObj->content;
             }
-            
+
             $redmineApiResponse = $this->redmine->issue->create($redmineCreateIssueObj);
 
             return $redmineApiResponse;
