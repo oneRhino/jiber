70ec7b14ab72e06c1c4a163457d38d551f38be8b
Thaissa Mendes <thaissa.mendes@gmail.com>
2020-09-21 16:38:19 -0300

    Update Toggl profile URL

--- resources/views/users/settings_field.blade.php
+++ resources/views/users/settings_field.blade.php
@@ -14,7 +14,7 @@ switch ($boolean) {
 			@if ($boolean == 1)
 				<a href="{{ action('TogglController@import') }}" class="btn btn-success input-group-addon" data-toggle="tooltip" data-placement="bottom" title="This will import all your Workspaces, Clients, Projects and Tasks. Depending on how many records you have, it might take a while!">Import</a>
 			@elseif ($boolean < 0)
-				<p><a href="https://toggl.com/app/profile" target="_blank">https://toggl.com/app/profile</a></p>
+				<p><a href="https://track.toggl.com/profile" target="_blank">https://track.toggl.com/profile</a></p>
 			@endif
 		@elseif ($name == 'redmine')
 			@if ($boolean < 0)
